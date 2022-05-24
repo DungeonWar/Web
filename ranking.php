93dfa4ec02ad53b0b870d4792b904efa7d675bb4
DungeonWar <106108374+DungeonWar@users.noreply.github.com>
2022-05-24 12:04:00 +0200

    Add files via upload

--- ranking.php
+++ ranking.php
@@ -9,10 +9,8 @@ for ($i=0; $i < 10 ; $i++) {
     $nombres = $statement[0];
 
     $usr[$i] = $conexion->query("SELECT * FROM ranking WHERE username = '$nombres'")->fetch();
-}
 
-/* $statement4 = $conexion->query("SELECT ID FROM noticias ORDER BY ID DESC LIMIT $i,1")->fetch();
-    $noticia[$i] = $conexion->query("SELECT * FROM noticias WHERE ID = $statement4[0]")->fetch();*/
+}
 
 require "vistas/ranking.view.php" ;
 ?>
